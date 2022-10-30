47328762f48f46b0799c9e5494fdb611b0340247
Elliot Lister <s1972614@ed.ac.uk>
2022-10-30 09:07:25 +0000

    Session 1 solutions

--- game.js
+++ game.js
@@ -11,6 +11,20 @@ const SETTINGS = {
     paddleSound: '/Sounds/Paddle.wav',
     wallSound: '/Sounds/Wall.wav',
     scoreSound: '/Sounds/Score.wav',
+
+    fps: 60,
+    courtColour: "black",
+    wallColour: "white",
+    wallSize: 20,
+    courtMarginX: 12,
+    courtMarginY: 4,
+
+    width: innerWidth,
+    height: innerHeight,
+
+    paddleColour: "white",
+    paddleWidth: 12,
+    paddleHeight: 48,
 }
 
 const PLAYERS = {
@@ -19,6 +33,34 @@ const PLAYERS = {
 }
 
 class Game {
+
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.court = new Court(canvas);
+    }
+
+    start() {
+        let that = this;
+        let previousTime = Date.now();
+
+        setInterval(function() {
+
+            let now = Date.now()
+            let dT = (now - previousTime) / 1000.0
+
+            that.draw()
+
+            previousTime = now
+
+        }, 1/SETTINGS.FPS * 1000)
+    }
+
+    draw() {
+        let ctx = this.canvas.getContext("2d")
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+        this.court.draw(this.canvas)
+    }
     
 }
 
@@ -27,7 +69,22 @@ class ScoreBoard {
 }
 
 class Paddle {
-    
+    constructor(x, y, width, height, player, court) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.player = player
+        this.court = court
+        this.startX = x
+        this.startY = y
+    }
+
+    draw(canvas){
+        let ctx = canvas.getContext('2d')
+        ctx.fillStyle = SETTINGS.paddleColour
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
 }
 
 class PaddleController {
@@ -39,11 +96,72 @@ class AIController {
 }
 
 class Court {
-    
+    constructor(canvas) {
+        this.canvas = canvas
+        
+        this.leftPaddle = new Paddle(
+            SETTINGS.paddleWidth,
+            canvas.height / 2 - SETTINGS.paddleHeight / 2,
+            SETTINGS.paddleWidth,
+            SETTINGS.paddleHeight,
+            PLAYERS.playerOne,
+            this
+        )
+
+        this.rightPaddle = new Paddle(
+            canvas.width - 2 * SETTINGS.paddleWidth,
+            canvas.height / 2 - SETTINGS.paddleHeight / 2,
+            SETTINGS.paddleWidth,
+            SETTINGS.paddleHeight,
+            PLAYERS.playerTwo,
+            this
+        )
+
+        this.ball = new Ball(canvas.width/2, canvas.height/2, 10, this)
+    }
+
+    draw(canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.fillStyle = SETTINGS.courtColour
+        ctx.fillRect(0, 0, SETTINGS.width, SETTINGS.height)
+
+        ctx.fillStyle = SETTINGS.wallColour
+        // Draw upper wall
+        ctx.fillRect(0, SETTINGS.courtMarginY, this.canvas.width, SETTINGS.wallSize)
+        // Draw lower wall
+        ctx.fillRect(0, this.canvas.height - SETTINGS.courtMarginY - SETTINGS.wallSize, this.canvas.width, SETTINGS.wallSize)
+
+        // Dashed Center Line
+        ctx.setLineDash([40]);
+        ctx.beginPath();
+        ctx.moveTo(SETTINGS.width/2, SETTINGS.courtMarginY);
+        ctx.lineTo(SETTINGS.width/2, SETTINGS.height - SETTINGS.courtMarginY);
+        ctx.strokeStyle = SETTINGS.wallColour;
+        ctx.stroke();
+
+        this.leftPaddle.draw(canvas)
+        this.rightPaddle.draw(canvas)
+
+        this.ball.draw(canvas)
+    }
 }
 
 class Ball {
-    
+    constructor (x, y, radius, court) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.court = court
+    }
+
+    draw(canvas){
+        let ctx = canvas.getContext("2d")
+
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI)
+        ctx.fillStyle = "white"
+        ctx.fill()
+    }
 }
 
 class Rectangle {
@@ -76,4 +194,6 @@ class Rectangle {
 const canvas = document.getElementById("game")
 canvas.width = SETTINGS.width;
 canvas.height = SETTINGS.height;
-let game = new Game(canvas)
+let game = new Game(canvas);
+
+game.start();
